fd0ce819c09da63df7fd96518ac2b98862a33671
jhf <3426628936@qq.com>
2024-04-30 16:50:55 +0800

    a

---
+++ vue-project/src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+import 'virtual:windi.css'
+app.use(router)
+
+app.mount('#app')
